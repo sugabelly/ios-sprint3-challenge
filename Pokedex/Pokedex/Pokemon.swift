2f6cb069e355d1d2aa86a915adc8c45c9720d936
Lotanna <sugabelly@gmail.com>
2018-11-02 16:08:14 -0400

    error fixes

--- Pokedex/Pokedex/Pokemon.swift
+++ Pokedex/Pokedex/Pokemon.swift
@@ -9,23 +9,51 @@
 import Foundation
 import UIKit
 
-struct Pokemon: Equatable, Codable {
+struct Pokemon: Codable, Equatable {
     
-    var name: String
-    var type: [PokemonType]
-    var abilities: [Ability]
-    let id: Int
-    let image: Data
+    var name: String //From API
+    let types: [TypeElement] //From API
+    let abilities: [Ability] //From API
+    var id: Int //From API
+    let sprites: Sprites //From API
+    var image: Data //Not from API
     
+    enum CodingKeys: String, CodingKey {
+        case name
+        case types
+        case abilities
+        case id
+        case sprites
+    }
     
     //Structs for the API information
     
-    struct Ability: Equatable, Codable {
+    struct Species: Codable {
         let name: String
     }
     
-    struct PokemonType: Equatable, Codable {
-        let name: String
+    struct TypeElement: Codable {
+        let slot: Int
+    }
 
+    struct Ability: Codable {
+        let ability: Species
+        
+        enum CodingKeys: String, CodingKey {
+            case ability
+        }
+    }
+    
+    struct Sprites: Codable {
+        let frontDefault: String
+        
+        enum CodingKeys: String, CodingKey {
+            case frontDefault = "front_default"
+        }
+    }
+    
+    //Conforming to Equatable
+    static func == (lhs: Pokemon, rhs: Pokemon) -> Bool {
+        return lhs.id == rhs.id
     }
 }
